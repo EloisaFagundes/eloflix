f3e1a0615b12a9235fb1c7d789e01cd18dd77e80
Eloisa Fagundes <lola.fernanda@hotmail.com>
2020-07-27 23:21:33 -0300

    adiciona arquivo de estilos

---
+++ eloflix-project/src/components/Menu/Styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const LogoStyled = styled.img`
+  height: 70px;
+`;
+
+export const MenuStyled = styled.nav`
+  width: 100%;
+  min-height: 94px;
+  z-index: 100;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding-left: 5%;
+  padding-right: 5%;
+  background: var(--black);
+  border-bottom: 2px solid var(--primary);
+  body {
+    --bodyPaddingTop: 94px;
+    padding-top: var(--bodyPaddingTop);
+  }
+  @media (max-width: 800px) {
+      height: 70px;
+      justify-content: center;
+    body {
+        --bodyPaddingTop: 40px;
+        padding-top: var(--bodyPaddingTop);
+    }
+  }
+`;
+
