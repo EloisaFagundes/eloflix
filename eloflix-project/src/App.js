9d3b3a41dfc19e26ed80108faf4875923f80b53e
Eloisa Fagundes <lola.fernanda@hotmail.com>
2020-07-27 23:20:00 -0300

    atualiza os logos

--- eloflix-project/src/App.js
+++ eloflix-project/src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import Menu from './components/Menu/Menu';
+import React from "react";
+import Menu from "./components/Menu/Menu";
+import dadosIniciais from "./data/dados_iniciais.json";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
-    <Menu />
-    </>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={
+          "Se você tem um felino caçador de aventuras, esse é o vídeo certo! Seu gatinho vai ficar entretido horas tendo descobrir de onde vem os pássaros e para onde eles vão!"
+        }
+      />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+      <Carousel category={dadosIniciais.categorias[1]} />
+
+      <Carousel category={dadosIniciais.categorias[2]} />
+
+      <Carousel category={dadosIniciais.categorias[3]} />
+
+      <Carousel category={dadosIniciais.categorias[4]} />
+
+      <Carousel category={dadosIniciais.categorias[5]} />
+
+      <Footer />
+    </div>
   );
 }
 
